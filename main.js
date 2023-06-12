df92949e84825afaa1eb687591ec421de87f6210
JuandaMT <juandamt@outlook.es>
2023-06-12 15:33:40 +0200

    extra work finished

--- main.js
+++ main.js
@@ -106,5 +106,95 @@ platosVeganos.forEach((plato) => {
 /* REDUCE */
 // 1
 const numeros = [39, 2, 4, 25, 62];
-const multiplicarNums = numeros.reduce((a, b) => {return a*b});
-console.log(multiplicarNums)
+const multiplicarNums = numeros.reduce((a, b) => {
+  return a * b;
+});
+console.log(multiplicarNums);
+
+/* EXTRAS */
+// 1
+const staff = [
+  {
+    name: "Pepe",
+    role: "The Boss",
+    hobbies: ["leer", "ver pelis"],
+  },
+  {
+    name: "Ana",
+    role: "becaria",
+    hobbies: ["nadar", "bailar"],
+  },
+  {
+    name: "Luis",
+    role: "programador",
+    hobbies: ["dormir", "comprar"],
+  },
+  {
+    name: "Carlos",
+    role: "secretario",
+    hobbies: ["futbol", "queso"],
+  },
+];
+
+const roles = staff.map((persona) => {
+  return `${persona.name} es ${persona.role} y le gusta ${persona.hobbies[0]} y ${persona.hobbies[1]}`;
+});
+console.log(roles);
+// 2
+const foodList2 = ["Pizza", "Ramen", "Paella", "Entrecot"];
+const comida = foodList2.map((plato) => {
+  switch (plato) {
+    case "Pizza":
+      console.log(`Como soy de Italia, amo comer Pizza`);
+      break;
+    case "Ramen":
+      console.log(`Como soy de Japón, amo comer Ramen`);
+      break;
+    case "Paella":
+      console.log(`Como soy de Valencia, amo comer Paella`);
+      break;
+    case "Entrecot":
+      console.log(`Aunque no como carne, el Entrecot es sabroso`);
+      break;
+  }
+});
+
+/* FILTER */
+const inventory = [
+  {
+    name: "Mobile phone",
+    price: 199,
+  },
+  {
+    name: "TV Samsung",
+    price: 459,
+  },
+  {
+    name: "Viaje a cancún",
+    price: 600,
+  },
+  {
+    name: "Mascarilla",
+    price: 1,
+  },
+];
+
+// 1
+const mayorDe300 = inventory.filter((objeto) => objeto.price > 300);
+console.log(mayorDe300);
+// 2
+const sentenceElements = [
+  "Me",
+  "llamo",
+  "Juanda",
+  "y",
+  "quiero",
+  "sentir",
+  "la",
+  "fuerza",
+  "con",
+  "javascript",
+];
+
+const phrase = sentenceElements.reduce((a, b) => a + " " + b);
+console.log(phrase);
